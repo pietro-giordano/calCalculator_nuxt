ded4f8578d9df88fdc343c68b693929e0b08072c
Pietro Giordano <giopi87@gmail.com>
2023-10-11 13:57:10 +0200

    tailwind integrato

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+      content: [
+            "./components/**/*.{js,vue,ts}",
+            "./layouts/**/*.vue",
+            "./pages/**/*.vue",
+            "./plugins/**/*.{js,ts}",
+            "./app.vue",
+            "./error.vue",
+      ],
+      theme: {
+            extend: {},
+      },
+      plugins: [],
+};
