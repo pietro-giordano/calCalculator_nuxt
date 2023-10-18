759bdf80502d3789490a30caed6932d55d0846d9
Pietro Giordano <giopi87@gmail.com>
2023-10-18 19:24:21 +0200

    navbar sistemata, responsive

--- components/navbar.client.vue
+++ components/navbar.client.vue
@@ -36,17 +36,36 @@
                   </div>
             </div>
       </nav> -->
-      <nav class="flex lg:px-16 px-6 lg:py-0 py-2 bg-white shadow-md">
-            <div>
-                  <a href="/" class="flex items-center text-lg font-semibold">
-                        <img src="../public/559birthdaycake3_100233.svg" width="50" height="50" class="p-2"
-                              alt="Rz Codes Logo" />
+      <nav class="flex flex-col lg:flex-row lg:px-16 px-6 lg:py-2 py-2 bg-white shadow-md">
+            <div class="flex justify-between lg:justify-normal">
+                  <a href="#" class="flex items-center text-lg font-semibold">
+                        <img src="../public/559birthdaycake3_100233.svg" width="50" height="50" alt="Rz Codes Logo" />
                         <div class="text-red-600 text-xl px-3 align-middle">calCalculator</div>
                   </a>
+
+                  <!-- hamburger by tailwind components -->
+                  <div class="flex items-center lg:hidden">
+                        <button class="text-red-600 w-10 h-10 relative focus:outline-none bg-white" @click="open = !open">
+                              <span class="sr-only">Open main menu</span>
+                              <div class="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
+                                    <span aria-hidden="true"
+                                          class="block absolute h-0.5 w-6 bg-current transform transition duration-500 ease-in-out"
+                                          :class="{ 'rotate-45': open, ' -translate-y-1.5': !open }"></span>
+                                    <span aria-hidden="true"
+                                          class="block absolute  h-0.5 w-6 bg-current   transform transition duration-500 ease-in-out"
+                                          :class="{ 'opacity-0': open }"></span>
+                                    <span aria-hidden="true"
+                                          class="block absolute  h-0.5 w-6 bg-current transform  transition duration-500 ease-in-out"
+                                          :class="{ '-rotate-45': open, ' translate-y-1.5': !open }"></span>
+                              </div>
+                        </button>
+                  </div>
+                  <!-- hamburger end -->
             </div>
 
-            <div class="flex flex-1 justify-between ps-6">
-                  <ul class="flex items-center text-lg gap-x-5">
+            <div class="lg:flex flex-col lg:flex-row flex-1 justify-between lg:ps-6 pt-2 lg:pt-0"
+                  :class="!open ? 'hidden' : 'flex'">
+                  <ul class="flex flex-col lg:flex-row items-center text-lg gap-x-5">
                         <li v-for="link in navbar" class="py-2 lg:py-0">
                               <a class="text-red-600 hover:pb-4 hover:border-b-4 hover:border-yellow-400 uppercase"
                                     :href=link.linkTo>
@@ -55,7 +74,7 @@
                         </li>
                   </ul>
 
-                  <ul class="text-xl text-center items-center gap-x-5 pt-4 md:gap-x-4 lg:text-lg lg:flex lg:pt-0">
+                  <ul class="text-xl text-center items-center justify-center gap-x-4 lg:text-lg flex py-3 lg:py-0">
                         <button1 content="Sign Up" />
                         <button2 content="Login" />
                   </ul>
@@ -64,6 +83,8 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+const open = ref(false);
 
 interface NavLink {
       text: string,
